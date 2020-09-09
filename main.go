9c021d3c197e5c5413e5072e031b175f8ec3b9eb
Tom Bierschenk <tom.bierschenk@auxilit.de>
2020-09-09 13:12:09 +0200

    added update and delete functions

--- main.go
+++ main.go
@@ -34,8 +34,10 @@ func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/new-booking", createNewBooking).Methods("POST")
-	myRouter.HandleFunc("/all-bookings", returnAllBookings)
-	myRouter.HandleFunc("/booking/{id}", returnSingleBooking)
+	myRouter.HandleFunc("/all-bookings", returnAllBookings).Methods("GET")
+	myRouter.HandleFunc("/booking/{id}", returnSingleBooking).Methods("GET")
+	myRouter.HandleFunc("/update-post/{id}", updateBooking).Methods("PUT")
+	myRouter.HandleFunc("/delete/{id}", deleteBooking).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
@@ -48,7 +50,6 @@ func createNewBooking(w http.ResponseWriter, r *http.Request) {
 	var booking Booking
 	json.Unmarshal(reqBody, &booking)
 	fmt.Printf("%+v\n", booking)
-	//fmt.Println("ID: " + booking.id + "User: " + booking.User + "Members: " + booking.Members)
 	db.Create(&booking)
 }
 
@@ -63,18 +64,52 @@ func returnSingleBooking(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["id"]
 	fmt.Println("Endpoint Hit: Booking No: ", key)
-	bookings := []Booking{}
-	db.Find(&bookings)
 
-	for _, booking := range bookings {
-		// string to int
-		s, err := strconv.Atoi(key)
-		if err == nil {
-			if booking.ID == s {
-				fmt.Println(booking)
-				json.NewEncoder(w).Encode(booking)
-			}
+	s, err := strconv.Atoi(key)
+	if err == nil {
+		var booking Booking
+		db.First(&booking, s)
+		fmt.Println(booking)
+		json.NewEncoder(w).Encode(booking)
+	}
+}
+
+func updateBooking(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	var booking Booking
+	json.Unmarshal(reqBody, &booking)
+	fmt.Printf("%+v\n", booking)
+
+	vars := mux.Vars(r)
+	key := vars["id"]
+	fmt.Println("Endpoint Hit: Update Booking No: ", key)
+
+	s, err := strconv.Atoi(key)
+	if err == nil {
+		// only update if id from path and json match
+		if s == booking.ID {
+			var dbBooking Booking
+			db.Where("ID = ?", s).First(&dbBooking)
+			dbBooking.User = booking.User
+			dbBooking.Members = booking.Members
+			db.Save(&dbBooking)
+			fmt.Println(dbBooking)
+			json.NewEncoder(w).Encode(dbBooking)
 		}
+	}
+}
+
+// delete a booking with the given id
+func deleteBooking(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	fmt.Println("Endpoint Hit: Delete Booking No: ", key)
+	s, err := strconv.Atoi(key)
+	if err == nil {
+		var booking Booking
+		db.First(&booking, s)
+		db.Delete(&booking)
 	}
 }
 
